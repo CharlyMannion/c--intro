865a84affe9a42033c1e6963f277818e823572d2
Charly Mannion <charlottemannion100@gmail.com>
2021-01-18 10:56:22 +0000

    plays around with creating classes with methods

---
+++ intro/Katas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TypesIntro
+{
+    public class Katas
+    {
+        public string ConvertToSassCase()
+        {
+            string greeting = "hello";
+            return greeting;
+        }
+
+        public double ReturnDouble()
+        {
+            var num = 0.5;
+            return num;
+        }
+    }
+}
